b6672a250a29e71ff97ec60d72eff0156bcd039b
Mostafa Hamed <hamedmostafa726@gmail.com>
2026-09-24 15:59:37 +0300

    events

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,30 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      Name: "",
     };
+  },
+  methods: {
+    submitForm() {
+      event.preventDefault(); // to prevent the default behavior of the form submission, which is to refresh the page
+      // we can also use the @submit.prevent directive in the form tag to prevent the default behavior of the form submission
+      console.log("Submitted");
+      // here after page refreshes, the data will be lost, so we need to prevent the default behavior of the form submission
+    },
+    setName(event, lastname) {
+      this.Name = event.target.value + " " + lastname;
+      console.log(this.Name);
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+    },
+    resetInput() {
+      this.Name = "";
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
